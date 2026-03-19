3c267a9cd22004219f3f86e9695b876ab59a0c09
anjali <anjalitomerlin@yahoo.com>
2026-03-19 19:12:26 -0400

    added example section, minor polishing to other sections, reformatted code blocks throughout

--- modules/chapters/code_for_new_example.R
+++ modules/chapters/code_for_new_example.R
@@ -1,81 +1,92 @@
 pop_size <- 150000
 
 data <- read.csv("./modules/data/disease_flu_2017.csv")
-data<-mutate(data,
-  total_sum = rowSums(select(data, -WEEK, -TOTAL.SPECIMENS,-YEAR))
-)
-data<-mutate(data,WEEK=((YEAR-2017)*52)+WEEK)
-data <- select(data,WEEK,total_sum)
-data <- mutate(data,WEEK=WEEK-40)
-data <- rename(data,day=WEEK,prevalence_count=total_sum)
+data <- mutate(data, total_sum = rowSums(select(data, -WEEK, -TOTAL.SPECIMENS, -YEAR)))
+data <- mutate(data, WEEK = ((YEAR - 2017) * 52) + WEEK)
+data <- select(data, WEEK, total_sum)
+data <- mutate(data, WEEK = WEEK - 40)
+data <- rename(data, day = WEEK, prevalence_count = total_sum)
 sum(data$prevalence_count)
 
-ModelFunc <- function(time,state,parameters){
+ModelFunc <- function(time, state, parameters) {
   S <- state[1]
   I <- state[2]
   R <- state[3]
+  
   
   beta <- parameters["beta"]
   gamma <- parameters["gamma"]
   N <- parameters["N"]
   
-  dS <- -(beta*I*S)/N
-  dI <- (beta*I*S)/N - (gamma*I)
-  dR <- (gamma*I)
+  dS <- -(beta * I * S) / N
+  dI <- (beta * I * S) / N - (gamma * I)
+  dR <- (gamma * I)
   
-  return(c(dS,dI,dR))
+  return(c(dS, dI, dR))
 }
 
 # Function Inputs: Named vector of model parameters and a vector of times
 # Function Outputs: Predicted prevalences at input times
-pred <- function(parameters,t){
-  initial_vals <- c(S=parameters["N"]-data[1,2],I=data[1,2],R=0)
+pred <- function(parameters, t) {
+  initial_vals <- c(S = parameters["N"] - data[1, 2], I = data[1, 2], R =
+                      0)
   t_start <- 0
   t_end <- max(t)
   step_size <- t_end - t_start
   
   # Calling rk4sys to solve our model equations
-  output <- rk4sys(f=function(t,y) ModelFunc(t,y,parameters),
-                   a=t_start,
-                   b=t_end,
-                   y0=initial_vals,
-                   n=step_size)
+  output <- rk4sys(
+    f = function(t, y)
+      ModelFunc(t, y, parameters),
+    a = t_start,
+    b = t_end,
+    y0 = initial_vals,
+    n = step_size
+  )
   
   # Extracting predicted prevalences
   output_df <- as.data.frame(output)
   prevalences <- output_df$y.I[t]
   
   # Ensuring there are no NaNs or out of bounds numbers
-  prevalences[is.nan(prevalences) | prevalences < 0 ] <- 0
+  prevalences[is.nan(prevalences) | prevalences < 0] <- 0
   
   return(prevalences)
 }
 
 # Function Inputs: A named list containing a value for beta
 # Function Outputs: The log-likelihood of the data given the input beta
-binomial_ll <- function(b){
+binomial_ll <- function(b) {
   times <- data$day
-  parameters <- c(beta=b,gamma=1.4,N=pop_size)
-  predicted <- pred(parameters,times)
+  parameters <- c(beta = b, gamma = 1.4, N = pop_size)
+  predicted <- pred(parameters, times)
   
-  # Returns the negative log-likelihood
-  -sum(dbinom(x=data$prevalence_count,
-              size = parameters["N"], 
-              prob = predicted/parameters["N"],
-              log=TRUE))
+  # Returns the negative log-likelihood-sum(
+  dbinom(
+    x = data$prevalence_count,
+    size = parameters["N"],
+    prob = predicted / parameters["N"],
+    log = TRUE
+  )
+  )
 }
 
-start <- list(b=2)
+start <- list(b = 2)
 
-# Using the L-BFGS-B algorithm / method for MLE 
+# Using the L-BFGS-B algorithm / method for MLE
 # Setting the lower bound to be small but positive since beta is non-negative
-result <- mle2(minuslogl=binomial_ll,
-               start=start,
-               method="L-BFGS-B",
-               lower=c(b=0.1),upper=c(b=3))
+result <- mle2(
+  minuslogl = binomial_ll,
+  start = start,
+  method = "L-BFGS-B",
+  lower = c(b = 0.1)
+)
 
 # Printing our estimate for beta
-Model <- function(time,state){
+
+coef(result)
+
+Model <- function(time, state) {
   # Extracting the state variables: susceptible, infected, and recovered (S,I,R)
   S <- state[1]
   I <- state[2]
@@ -88,12 +99,12 @@ Model <- function(time,state){
   gamma <- 1.4
   
   # Constructing our SIR model equations
-  dS <- -(beta*I*S)/pop_size
-  dI <- (beta*I*S)/pop_size - (gamma*I)
-  dR <- (gamma*I)
+  dS <- -(beta * I * S) / pop_size
+  dI <- (beta * I * S) / pop_size - (gamma * I)
+  dR <- (gamma * I)
   
   # Now we will return those model equations as a vector
-  return(c(dS,dI,dR))
+  return(c(dS, dI, dR))
 }
 
 
@@ -103,7 +114,7 @@ end <- 51
 
 # Initial values to be passed into the parameter y
 # Make sure values line up with the state variables in the model function
-initial_vals <- c(S=pop_size-171,I=171,R=0)
+initial_vals <- c(S = pop_size - 171, I = 171, R = 0)
 
 # How many steps to take from a to b
 step_size <- 100
@@ -111,11 +122,13 @@ step_size <- 100
 library(pracma)
 
 # Make sure the values we defined are being passed into the correct arguments
-output <- rk4sys(f=Model,
-                 a=start,
-                 b=end,
-                 y0=initial_vals,
-                 n=step_size)
+output <- rk4sys(
+  f = Model,
+  a = start,
+  b = end,
+  y0 = initial_vals,
+  n = step_size
+)
 
 # Converting the output matrix to a data frame and printing it
 output <- as.data.frame(output)
@@ -127,13 +140,27 @@ R <- output$y.R
 beta <- 1.15
 gamma <- 1.4
 View(output)
-plot(times, I, type = "l", col = "steelblue", lwd = 2, xlab = "Time", ylab = "Proportion",
-     main = paste0("SIR Epidemic (Runge-Kutta)  |  β=", beta,
-                   "  γ=", gamma, "  R₀=", round(beta/gamma, 2)))
+plot(
+  times,
+  I,
+  type = "l",
+  col = "steelblue",
+  lwd = 2,
+  xlab = "Time",
+  ylab = "Proportion",
+  main = paste0(
+    "SIR Epidemic (Runge-Kutta)  |  β=",
+    beta,
+    "  γ=",
+    gamma,
+    "  R₀=",
+    round(beta / gamma, 2)
+  )
+)
 
-points(data[1:nrow(data),]$day,data[1:nrow(data),]$prevalence_count)
+points(data[1:nrow(data), ]$day, data[1:nrow(data), ]$prevalence_count)
 
 unname(coef(result))[1]
 
-write.csv(data,"disease_flu_2017_MODIFIED.csv")
+write.csv(data, "disease_flu_2017_MODIFIED.csv")
 writ
--- shiny-apps/eff_reproduction/eff_reprod_solver.R
+++ shiny-apps/eff_reproduction/eff_reprod_solver.R
@@ -11,7 +11,6 @@ solve_sir <- function(S0, I0, R0, b, g, len, dt) {
     })
   }
   init <- c(S=S0, I= I0, R= R0)
-  ## Original parameters for the nice curve: parameters <- c(beta = 1.4247, gamma = 0.14286)
   parameters <- c(beta=b, gamma=g)
   times <- seq(0, len, by = dt)
   out <- as.data.frame(ode(y = init, times = times, func = sir, parms = parameters))
